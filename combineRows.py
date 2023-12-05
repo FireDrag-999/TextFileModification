5e72f7fa9e17a8ea9bb17b53c6158e9d0561fee7
FireDrag <100155709+FireDrag-999@users.noreply.github.com>
2023-12-05 10:44:30 +0000

    Comments

--- combineRows.py
+++ combineRows.py
@@ -1,10 +1,10 @@
 import os
 import pathlib
 
-folderName = input("What is the name of the folder containing the files: ")
-
-currentDir = str(pathlib.Path(__file__).parent.resolve())
-fullFolderPath = currentDir + "/" + folderName + "/"
+folderName = input("What is the name of the folder containing the files: ") # script must be placed outside of the folder e.g. script, "records folder" in the same folder
+                                                                            # All text files in a folder are combined in the order stored in the folder                                                                  
+currentDir = str(pathlib.Path(__file__).parent.resolve())                   # Creates a file inside the folder called "!All (foldername).txt"   
+fullFolderPath = currentDir + "/" + folderName + "/"                        # To run again the "!All (foldername).txt" file must be moved/deleted from the folder
 
 if not os.path.isdir(fullFolderPath):
     print("That folder does not exist")
@@ -14,13 +14,13 @@ folderDir = os.listdir(fullFolderPath)
 fileNumInFolder = len(folderDir)
 
 try:
-    newFile = open(fullFolderPath + "All " + folderName + ".txt", "x")
+    newFile = open(fullFolderPath + "!All " + folderName + ".txt", "x")
 except FileExistsError:
     print("File already exists")
     quit()
 
-for i in folderDir:
-    readFile = open(fullFolderPath + i, "r")
+for file in folderDir:
+    readFile = open(fullFolderPath + file, "r")
     fileData = readFile.readlines()
     newFile.writelines(fileData)
     newFile.writelines("\n")
--- splitRows.py
+++ splitRows.py
@@ -2,9 +2,9 @@ import math
 import os
 import pathlib
 
-fileName = input("What is the filename (excluding .txt): ")
-rowCount = int(input("How many rows per file: "))
-fileNum = 0
+fileName = input("What is the filename (excluding .txt): ") # file must be placed in the same folder as the script
+rowCount = int(input("How many rows per file: "))           # creates a folder with the file's name and places the new files under it
+fileNum = 0                                                 # To rerun either the files in the folder must be moved or deleted
 currentDir = str(pathlib.Path(__file__).parent.resolve())
 fullFolderPath = currentDir + "/" + fileName + "/"
 
